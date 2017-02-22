5865b81453a41981a1942992dbfc2631cdd31ecb
shifan <shz144@ucsd.edu>
2017-02-22 14:50:34 -0800

    2nd

---
+++ README.md
@@ -0,0 +1,1 @@
+ld Status](https://travis-ci.org/sssky/Lab6.svg?branch=master)](https://travis-ci.org/sssky/Lab6)ld Status](https://travis-ci.org/sssky/Lab6.svg?branch=master)](https://travis-ci.org/sssky/Lab6)
---
+++ src/edu/ucsd/cs110/temperature/Temperature.java
@@ -0,0 +1,19 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by shifanzhang on 2/15/17.
+ */
+public abstract class Temperature {
+    private float value;
+
+    public Temperature(float v)
+    {
+        value = v;
+    }
+    public final float getValue()
+    {
+        return value;
+    }
+    public abstract Temperature toCelsius();
+    public abstract Temperature toFahrenheit();
+}
